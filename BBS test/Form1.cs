e7f7f6ad94d8e55f95d48b86d10a9bc22a83ff07
SatellightsJimin <plaguefor@gmail.com>
2014-07-23 18:04:39 +0900

    윈폼

---
+++ BBS test/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BBS_test
+{
+   
+    public partial class Form1 : Form
+    {
+        BBSControl bbsControl;
+        DataSet dataSet;
+        public Form1()
+        {
+            InitializeComponent();
+            bbsControl = new BBSControl();
+            dataSet = bbsControl.GetContents(1, 5);
+  
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void writeBtn_Click(object sender, EventArgs e)
+        {
+            Form2 writeForm = new Form2();
+            writeForm.Owner = this;
+            writeForm.Show();
+        }
+
+
+    }
+    public class BBSControl
+    {
+        public DataSet GetContents(int pageno, int pagesize)
+        {
+            DataSet ds = new DataSet();
+            string strConn = "Data Source=D306;Initial Catalog=MyTestDB;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+            string sql
+            = "select a.rownum, a.ID, a.USER_NAME, a.SUBJECT, a.DATE from (select row_number() OVER (ORDER BY DATE DESC) as rownum, ID, USER_NAME, SUBJECT, DATE from MyBBS) as a where a.rownum between (@pageno * (@pageno-1)) and ((@pageno * (@pageno-1)) + @pagesize)";
+            SqlConnection conn = new SqlConnection(strConn);
+            conn.Open();
+
+            SqlParameter paramNo = new SqlParameter("@pageno", SqlDbType.Int);
+            SqlParameter paramSize = new SqlParameter("@pagesize", SqlDbType.Int);
+            paramNo.Value = pageno;
+            paramSize.Value = pagesize;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.Add(paramNo);
+            cmd.Parameters.Add(paramSize);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ds);
+
+            conn.Close();
+            return ds;
+        }
+    }
+}
---
+++ BBS test/Form2.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BBS_test
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(140, 278);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(75, 23);
+            this.submitBtn.TabIndex = 0;
+            this.submitBtn.Text = "작성";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 205);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(101, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(55, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(288, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "제목 :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "이름 :";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 312);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.submitBtn);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
